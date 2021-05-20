8bf4b1e685140351f0acdbfc34e0ac6f097a53b3
Otoniel2514 <81173407+Otoniel2514@users.noreply.github.com>
2021-05-19 17:00:19 -0700

    Upload

---
+++ Ejercicio8Tarea.cpp
@@ -0,0 +1,62 @@
+//Hacer un programa que calcule los aportes de ley y crear un menu que le permita al usuario conocer su descuento de acuerdo a la opcion seleccionada
+
+#include "iostream"
+#include "math.h"
+#include "iomanip"
+using namespace std;
+int main()
+{
+    int opcion;
+    
+    //Pedir al usuario que ingrese su sueldo
+     cout<<"Ingrese la cantidad de su sueldo: "<<endl;
+     cin>>opcion;
+
+    //Mostrar el menu del sistema al usuario
+    
+        cout<<"========MENU DE OPCIONES DEL SISTEMA========"<<endl;
+        cout<<"\nBienvenido a su menu de consultas de descuentos de ley, sera un gusto atenderle."<<endl;
+        cout<<"Ingrese la opcion que desea realizar en el sistema."<<endl;
+        cout<<"1. Calcular mis aportes de Renta."<<endl;
+        cout<<"2. Calcular mis aportes de AFP."<<endl;
+        cout<<"3. Calcular mis aportes del ISSS."<<endl;
+        cout<<"4. Salir del sistema."<<endl;
+        cout<<"Ingrese la opcion que desea realizar en el sistema."<<endl;
+        cin>>opcion;
+
+        switch(opcion)
+    {
+        //Descuento de la renta
+        case 1:
+        float sueldo1, DesRenta;
+        cout << "Ingrese la cantidad de su sueldo: "<<endl;
+        cin >>sueldo1;
+        DesRenta =(sueldo1 * 0.10);
+        cout<<fixed<<setprecision(2);
+        cout << "El saldo a descontar de su sueldo en base al 10% de la Renta es: $" <<DesRenta<< endl;
+        break;
+
+        //Descuento de AFP
+        case 2:
+        float sueldo2, DesAFP;
+        cout << "Ingrese la cantidad de su sueldo: "<<endl;
+        cin >>sueldo2;
+        DesAFP =(sueldo2 * 6.25)*0.01;
+        cout<<fixed<<setprecision(2);
+        cout << "El saldo a descontar de su sueldo en base al 6.25% de AFP es: $" <<DesAFP<< endl;
+        break;
+
+        //Descuento de seguro ISSS
+        case 3:
+        float sueldo3, DesISSS;
+        cout<<"Ingrese la cantidad de su sueldo: "<<endl;
+        cin >>sueldo3;
+        DesISSS =(sueldo3 * 0.2);
+        cout<<fixed<<setprecision(2);
+        cout << "El saldo a descontar de su sueldo en base al 2% de ISSS es: $"<<DesISSS<< endl;
+        break;
+    }
+    return 0;
+}
+
+    
